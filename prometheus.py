6ee381ded09f5d5b323461bd841b701dde1d6085
Connor Demille <subtlepseudonym@gmail.com>
2023-08-05 19:56:48 -0400

    Only publish prom stats for connected sensors

--- prometheus.py
+++ prometheus.py
@@ -42,14 +42,23 @@ class Prometheus():
         )
 
     def update(self, m):
-        self.temperature_gauge.labels('dht22').set(m.dht_temperature)
-        self.temperature_gauge.labels('bme680').set(m.bme_temperature)
-        self.humidity_gauge.labels('dht22').set(m.dht_humidity / 100.0)
-        self.humidity_gauge.labels('bme680').set(m.bme_humidity / 100.0)
-        self.pressure_gauge.labels('bme680').set(m.pressure)
-        self.gas_resistance_gauge.labels('bme680').set(m.gas_resistance)
-        self.indoor_air_quality_gauge.labels('bme680').set(m.indoor_air_quality)
-        self.co2_gauge.labels('mhz19').set(m.co2)
+        if m.dht != None:
+            self.temperature_gauge.labels('dht22').set(m.dht_temperature)
+            self.humidity_gauge.labels('dht22').set(m.dht_humidity / 100.0)
+
+        if m.bme != None:
+            self.temperature_gauge.labels('bme680').set(m.bme_temperature)
+            self.humidity_gauge.labels('bme680').set(m.bme_humidity / 100.0)
+            self.pressure_gauge.labels('bme680').set(m.pressure)
+            self.gas_resistance_gauge.labels('bme680').set(m.gas_resistance)
+            self.indoor_air_quality_gauge.labels('bme680').set(m.indoor_air_quality)
+
+        if m.mhz != None:
+            self.temperature_gauge.labels('mhz19').set(m.mhz_temperature)
+            self.co2_gauge.labels('mhz19').set(m.mhz_co2)
+
+        if m.c8d != None:
+            self.co2_gauge.labels('c8d').set(m.c8d_co2)
 
     def serve(self, port=80):
         wlan = network.WLAN(network.STA_IF)
